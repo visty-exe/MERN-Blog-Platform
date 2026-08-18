9484daccac6989a4bddd6c9b35a50183eceddf30
Vishal Tyagi <vishal.tyagi14oct@gmail.com>
2026-08-18 22:40:58 +0530

    working on profie

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import "dotenv/config";
 
-
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -12,12 +11,17 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({
-    origin: ["http://localhost:5173",
-    "https://mern-blog-platform-hisp-cyan.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mern-blog-platform-hisp-cyan.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}));
+  })
+);
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -26,5 +30,5 @@ connectDB();
 app.use("/api", router);
 
 app.listen(PORT, () => {
-    console.log(`Server is running http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
