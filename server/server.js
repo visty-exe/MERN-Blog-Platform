d0ab99f1feb1c262671a2ef3fb6ba048f7e2b592
Vishal Tyagi <vishal.tyagi14oct@gmail.com>
2026-08-18 22:30:22 +0530

    working on profie

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors({
     origin: ["http://localhost:5173",
-    "https://mern-blog-platform-drab.vercel.app/"],
+    "https://mern-blog-platform-hisp-cyan.vercel.app/login"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
